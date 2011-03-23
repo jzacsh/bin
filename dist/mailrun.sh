3b3ae8a97299984b4da2ca79e6824c91397cbeda
Jonathan Zacsh <jzacsh@gmail.com>
2011-03-22 22:42:54 -0400

    timestamps for readable cron logging

--- dist/mailrun.sh
+++ dist/mailrun.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+printf 'started:\t%s\n' "$(date --rfc-3339=seconds)"
 [[ $(pgrep "offlineimap") ]] && pkill offlineimap || offlineimap -o -u Noninteractive.Quiet # &>/dev/null
+printf 'ended:\t%s\n' "$(date --rfc-3339=seconds)"
